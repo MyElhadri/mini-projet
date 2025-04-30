4b65a2feec02b32f151ce0ee8d27cca316ebecf1
akram <168238434+akramelassri@users.noreply.github.com>
2025-04-30 14:25:49 +0100

    Dashboard

---
+++ front/src/views/PeopleView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import  Dashboard  from "@/components/Dashboard.vue"
+
+</script>
+
+<template>
+    <Dashboard />
+    <div class="main-content">
+    <h1>People management</h1>
+</div>
+</template>
+
+<style scoped>
+</style>
